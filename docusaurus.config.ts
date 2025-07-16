b65919e3e7a5354a8fc21eaa76e1d8f6c821b1d6
Victor Florea <victorcristianflorea@gmail.com>
2025-07-16 11:54:55 +0300

    update gh pages config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,20 +5,20 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-	title: "Lunipump Docs",
-	tagline: "Lunitics Trading Docs",
+	title: "Monkets Docs",
+	tagline: "Monkets Docs",
 	favicon: "img/file_no_bg.svg",
 
 	// Set the production url of your site here
-	url: "https://your-docusaurus-site.example.com",
+	url: "https://vicimikul.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/monkets-docs/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
+	organizationName: "vicimikul", // Usually your GitHub org/user name.
+	projectName: "monkets-docs", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
@@ -52,7 +52,7 @@ const config: Config = {
 		// Replace with your project's social card
 		image: "img/docusaurus-social-card.jpg",
 		navbar: {
-			title: "Lunipump Docs",
+			title: "Monkets Docs",
 			logo: {
 				alt: "Lunipump Docs Logo",
 				src: "img/file_no_bg.svg",
